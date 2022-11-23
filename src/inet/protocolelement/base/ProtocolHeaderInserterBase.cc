16a2045a24594ec70dcc7f779e3f90eaed47f4a3
Levente Mészáros <levy@omnetpp.org>
2022-11-23 17:03:07 +0100

    protocolelement: Added ProtocolHeaderInserterBase class.

---
+++ src/inet/protocolelement/base/ProtocolHeaderInserterBase.cc
@@ -0,0 +1,12 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+
+#include "inet/protocolelement/base/ProtocolHeaderInserterBase.h"
+
+namespace inet {
+} // namespace inet
+
